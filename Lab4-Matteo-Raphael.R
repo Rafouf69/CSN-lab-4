6a2e0f54f6dc3a9a567834bafe9c2ef171496f67
Raphael Vignon <65215675+Rafouf69@users.noreply.github.com>
2022-11-08 21:33:38 +0100

    add plot

--- Lab4-Matteo-Raphael.R
+++ Lab4-Matteo-Raphael.R
@@ -7,14 +7,35 @@
 #=======================================================================
 # Note: Please set the working directory to the current folder
 
+#=======================================================================
+# Dependencies 
+#=======================================================================
+
+require(data.table)
+require(xtable)
+
+library(data.table)
+library(xtable)
+
+#=======================================================================
+# Functions 
+#=======================================================================
+
 write_summary <- function(language,file) {
-  degree_sequence = read.table(file, header = FALSE)
+  data = read.table(file, header = FALSE)
+  
+  colnames(data) <- c("vertices","degree_2nd_moment", "mean_length")
+  
+  valid_rows <- apply(data, 1, verification)
+  
+  data = data[valid_rows,]
+
   cat(
   	language,
-  	length(degree_sequence$V1),
-  	mean(degree_sequence$V1),
-  	length(degree_sequence$V2),
-  	mean(degree_sequence$V2),
+  	length(data$vertices),
+  	mean(data$vertices),
+  	length(data$degree_2nd_moment),
+  	mean(data$degree_2nd_moment),
   	"\n"
   )
 }
@@ -25,16 +46,23 @@ source = read.table("list_data.txt",
                     as.is = c("language","file") # this is need to have the cells treated as real strings and not as categorial data.
 )
 
-# verification fucntion
+# verification function
 
-k_verification <- function(n, k_2){
-  if(k_2 >= d - 6/n & k_2 <= n-1){
-    TRUE
-  } else {
-    FALSE
-  }
+verification <- function(row){
+	n <- row["vertices"]
+	k2 <- row["degree_2nd_moment"]
+	d <- row["mean_length"]
+	condition_k = (k2 >= 4-6/n && k2 <= n-1)
+	condition_d = (d >= n/(8*(n-1)) * k2 + 0.5 && d <= n-1)
+	return(condition_k && condition_d)
 }
 
 for (x in 1:nrow(source)) {
   write_summary(source$language[x], source$file[x])
+	
+	language = read.table(source$file[x], header = FALSE)
+	colnames(language) = c("vertices","degree_2nd_moment", "mean_length")
+	language = language[order(language$vertices), ]
+	plot(language$vertices, language$mean_length,
+			 xlab = "vertices", ylab = "mean dependency length")
 }
